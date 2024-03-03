2a7664ce81891b4c463734f081e256a13f5f7334
Serdar Kilic <serdar@kilic.net>
2024-03-03 14:53:11 +1100

    wip

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -2,8 +2,8 @@ import { getPosts } from "../../database/index";
 
 export const onRequest = async (context) => {
    const message = await getPosts(context.env.DATABASE_URL);
-
+   const {rows} = message
    console.log(JSON.stringify(message));
 
-   return new Response(JSON.stringify(message));
+   return new Response(JSON.stringify(rows));
 };
