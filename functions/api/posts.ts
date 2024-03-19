874eaef5b013e297bde20f458bb4ed5b4abe42f6
Serdar Kilic <serdar@kilic.net>
2024-03-20 01:08:54 +1100

    switched from planetscale to astrodb

--- functions/api/posts.ts
+++
@@ -1,9 +0,0 @@
-import { getPosts } from "../../database/index";
-
-export const onRequest = async (context) => {
-   const message = await getPosts(context.env.DATABASE_URL);
-   const {rows} = message
-   console.log(JSON.stringify(message));
-
-   return new Response(JSON.stringify(rows));
-};
