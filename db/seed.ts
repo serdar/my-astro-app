458be393ef23fcc3efc15947c46b942184105dad
Serdar Kilic <serdar@kilic.net>
2024-03-23 23:42:56 +1100

    wip

--- db/seed.ts
+++ db/seed.ts
@@ -32,7 +32,23 @@ export default async function seed() {
       description: "This is my 2 post",
       pubDate: new Date(),
       published: true,
-      slug: "my-2-post",
+      slug: "my-5-post",
+      siteVersionNumber: 1
+   },{
+      id: 5,
+      title: "My 5 Post",
+      description: "This is my 5 post",
+      pubDate: new Date(),
+      published: true,
+      slug: "my-5-post",
+      siteVersionNumber: 1
+   },{
+      id: 6,
+      title: "My 6 Post",
+      description: "This is my 6 post",
+      pubDate: new Date(),
+      published: true,
+      slug: "my-6-post",
       siteVersionNumber: 1
    }])
 }
