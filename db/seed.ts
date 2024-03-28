e2e3071adb0a0631930679b3deea3703d392a4cf
Serdar Kilic <serdar@kilic.net>
2024-03-28 13:05:00 +1100

    wip

--- db/seed.ts
+++ db/seed.ts
@@ -4,51 +4,10 @@ export default async function seed() {
    await db.insert(Comments).values([{
       id: 1,
       title: "My First Post",
-      description: "This is my first post",
+      description: "Blog post The coffee industry needs it's equivalent of the W3C to stop this mess:",
       pubDate: new Date(),
       published: true,
       slug: "my-first-post",
-      siteVersionNumber: 1
-   },{
-      id: 2,
-      title: "My Second Post",
-      description: "This is my 2 post",
-      pubDate: new Date(),
-      published: true,
-      slug: "my-2-post",
-      siteVersionNumber: 1
-   
-   },{
-      id: 3,
-      title: "My 3 Post",
-      description: "This is my 2 post",
-      pubDate: new Date(),
-      published: true,
-      slug: "my-2-post",
-      siteVersionNumber: 1
-   },{
-      id: 4,
-      title: "My 4 Post",
-      description: "This is my 2 post",
-      pubDate: new Date(),
-      published: true,
-      slug: "my-5-post",
-      siteVersionNumber: 1
-   },{
-      id: 5,
-      title: "My 5 Post",
-      description: "This is my 5 post",
-      pubDate: new Date(),
-      published: true,
-      slug: "my-5-post",
-      siteVersionNumber: 1
-   },{
-      id: 6,
-      title: "My 6 Post",
-      description: "This is my 6 post",
-      pubDate: new Date(),
-      published: true,
-      slug: "my-6-post",
       siteVersionNumber: 1
    }])
 }
